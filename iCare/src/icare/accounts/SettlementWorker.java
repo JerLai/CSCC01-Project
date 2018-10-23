cbb160154dc90bafb8f157a22b20be4ad1b89ba0
Bilal Syed <bilalhussain.syed@mail.utoronto.ca>
2018-10-23 02:10:29 -0400

    Completed skeleton structure of different User classes. Coded Admin ability to create/manage Accounts.

---
+++ iCare/src/icare/accounts/SettlementWorker.java
@@ -0,0 +1,13 @@
+package icare.accounts;
+
+public class SettlementWorker extends User{
+
+	public SettlementWorker(String username, String password, String firstName, String lastName, int iD) {
+		super(username, password, firstName, lastName, iD);
+	}
+
+	// To Do: Implement methods for creating new client entry
+
+	// To Do: Implements methods for assigning clients to specific organizations
+
+}
