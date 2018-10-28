ededfa39248095809c4046d0feb23791b1a0a19d
Bilal Syed <bilalhussain.syed@mail.utoronto.ca>
2018-10-28 16:22:32 -0400

    Completed export method to export data to csv files.

--- iCare/src/main/java/com/icare/database/databaseAPI.java
+++ iCare/src/main/java/com/icare/database/databaseAPI.java
@@ -2,19 +2,21 @@ package main.java.com.icare.database;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 import main.java.com.icare.passwords.passwords;
 
 public class databaseAPI{
 
 	private static final String DELIMITER = ",";
+	private static final String NEW_LINE_SEPARATOR = "\n";
 
 	public static boolean insertUser(Connection connection, String username, String password, String firstName, String lastName, String accountType) {
 
@@ -54,14 +56,14 @@ public class databaseAPI{
 		return false;
 	}
 
-	public static HashMap<String, ArrayList<String>> importData(String filename) {
+	public static LinkedHashMap<String, ArrayList<String>> importData(String fileName) {
 		BufferedReader csvReader = null;
-		HashMap <String, ArrayList<String>> fileData = new HashMap <String, ArrayList<String>>();
+		LinkedHashMap <String, ArrayList<String>> fileData = new LinkedHashMap <String, ArrayList<String>>();
 
 		try {
 			String headers = null;
 			String line = null;
-			csvReader = new BufferedReader(new FileReader("resources/" + filename));
+			csvReader = new BufferedReader(new FileReader("resources/" + fileName));
 
 			// read the first line to get the column headers
 			headers = csvReader.readLine();
@@ -85,13 +87,13 @@ public class databaseAPI{
 				}
 			}
 		} catch (Exception e) {
-			System.out.println("Error while reading .csv file: " + filename);
+			System.out.println("Error while reading .csv file: " + fileName);
 			e.printStackTrace();
 		} finally {
 			try {
 				csvReader.close();
 			} catch (IOException e) {
-				System.out.println("Error while closing .csv file: " + filename);
+				System.out.println("Error while closing .csv file: " + fileName);
 				e.printStackTrace();
 			}
 		}
@@ -99,8 +101,48 @@ public class databaseAPI{
 		return fileData;
 	}
 
-	public static void exportData(HashMap<String, ArrayList<String>> fileData) {
-		// TODO Auto-generated method stub
-		
+	public static void exportData(LinkedHashMap<String, ArrayList<String>> fileData, String fileName) {
+		FileWriter csvWriter = null;
+
+		try {
+			csvWriter = new FileWriter("resources/" + fileName);
+
+			// add column headers as first line in csv file
+			int i = 0;
+			for (String header : fileData.keySet()) {
+				csvWriter.append(header);
+				if (i < (fileData.keySet().size() - 1)) {
+					csvWriter.append(DELIMITER);
+				}
+				i++;
+			}
+			// add new line after column headers
+			csvWriter.append(NEW_LINE_SEPARATOR);
+
+			// iterate through each key-value pairing and write to file
+			for (int j = 0; j < fileData.keySet().size(); j++) {
+				int k = 0;
+				for (String header : fileData.keySet()) {
+					csvWriter.append(fileData.get(header).get(j));
+					k++;
+					if (k < (fileData.keySet().size())) {
+						csvWriter.append(DELIMITER);
+					}
+				}
+				if (j < (fileData.keySet().size() - 1)) {
+					csvWriter.append(NEW_LINE_SEPARATOR);
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Error while writing to .csv file: " + fileName);
+			e.printStackTrace();
+		} finally {
+			try {
+				csvWriter.close();
+			} catch (IOException e) {
+				System.out.println("Error while closing .csv file: " + fileName);
+				e.printStackTrace();
+			}
+		}
 	}
 }
