ac5975910da870430fdce9a295c1868e48c58343
Bilal Syed <bilalhussain.syed@mail.utoronto.ca>
2018-11-10 22:06:42 -0500

    Added import method and its dependencies.

--- iCare/src/main/java/com/icare/database/DatabaseIO.java
+++ iCare/src/main/java/com/icare/database/DatabaseIO.java
@@ -1,40 +1,46 @@
 package main.java.com.icare.database;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.*;
 
 public class DatabaseIO {
 
 	private static final String DELIMITER = ",";
 	private static final String NEW_LINE_SEPARATOR = "\n";
 
-	public static void importData(Connection connection, String fileName) {
+	public static void importData(Connection connection, File file) {
+		// get file name and decide table name
+        String fileName = file.getName();
 		String fileTable = fileName.substring(0, fileName.indexOf("."));
-		importData(connection, fileName, fileTable);
-	}
-
-	public static void importData(Connection connection, String fileName, String fileTable) {
-		BufferedReader csvReader = null;
+		// create workbook from Excel file
+		Workbook workbook = null;
+        try {
+        	workbook = WorkbookFactory.create(file);
+		} catch (Exception e) {
+			System.out.println("Error while reading file: " + fileName);
+			e.printStackTrace();
+		}
+        // first sheet
+        Sheet sheet = workbook.getSheetAt(0);
 
 		try {
-			String headers = null;
-			String headers2 = null;
-			String values = null;
+			String headers = "";
 			String tableColData = "ID INTEGER PRIMARY KEY NOT NULL";
-			csvReader = new BufferedReader(new FileReader("resources/" + fileName));
 
 			ArrayList<String> tableExistCols = new ArrayList<String>();
 			// check if table exists
 			try {
 				tableExistCols = (ArrayList<String>) databaseAPI.getTableColumnData(connection, fileTable);
 			} catch (Exception e) {
-				System.out.println("Error while checking if table exists for .csv file: " + fileName);
+				System.out.println("Error while checking if table exists for file: " + fileName);
 				e.printStackTrace();
 			}
 
@@ -43,14 +49,14 @@ public class DatabaseIO {
 			try {
 				oldTableExistCols = (ArrayList<String>) databaseAPI.getTableColumnData(connection, fileTable + "_old");
 			} catch (Exception e) {
-				System.out.println("Error while checking if _old table exists for .csv file: " + fileName);
+				System.out.println("Error while checking if _old table exists for file: " + fileName);
 				e.printStackTrace();
 			}
 			if (oldTableExistCols.size() > 0) {
 				try {
 					databaseAPI.deleteTable(connection, fileTable + "_old");
 				} catch (Exception e) {
-					System.out.println("Error while deleting _old table for .csv file: " + fileName);
+					System.out.println("Error while deleting _old table for file: " + fileName);
 					e.printStackTrace();
 				}
 			}
@@ -59,55 +65,73 @@ public class DatabaseIO {
 					databaseAPI.deleteTable(connection, fileTable + "_old");
 					databaseAPI.renameTable(connection, fileTable, fileTable + "_old");
 				} catch (Exception e) {
-					System.out.println("Error while saving _old table for .csv file: " + fileName);
+					System.out.println("Error while saving _old table for file: " + fileName);
 					e.printStackTrace();
 				}
 			}
 
-			// read the first line to get the column headers
-			headers = csvReader.readLine();
-			headers2 = headers;
-
-			// split the column headers
-			String[] columnHeaders = headers2.split(DELIMITER, -1);
-
-			// add column headers
-			for (String header : columnHeaders) {
-				tableColData += ", ";
-				tableColData += header;
-				tableColData += " char(255)";
+			// iterate through 3rd row to get column headers
+			Iterator<Row> rowIterator = sheet.rowIterator();
+			Row row = null;
+			Cell cell;
+			if (rowIterator.hasNext()) {
+				row = rowIterator.next();
 			}
+			if (rowIterator.hasNext()) {
+				row = rowIterator.next();
+			}
+			Iterator<Cell> cellIterator = row.cellIterator();
+			while (cellIterator.hasNext()) {
+                cell = cellIterator.next();
+                // determine column headers from 3rd row
+                tableColData += ", ";
+                String cellValue = cell.getRichStringCellValue().getString();
+                tableColData += cellValue;
+                tableColData += " char(255)";
+                headers += cellValue;
+                headers += ", ";
+			}
+			headers = headers.substring(0, headers.length()-2);
+
+			// create table with corresponding columns
 			try {
 				databaseAPI.createTable(connection, fileTable, tableColData);
 			} catch (Exception e) {
-				System.out.println("Error while inserting data for .csv file: " + fileName);
+				System.out.println("Error while inserting data for file: " + fileName);
 				e.printStackTrace();
 			}
 
-			// go through each line
-			while ((values = csvReader.readLine()) != null) {
-				// go through each line and add to table
-				try {
-					String[] valuesArray = values.split(DELIMITER, -1);
-					values = "";
-					for (String entry: valuesArray){
-						values += ", '" + entry + "'";
-					}
-					values = values.substring(2);
-					databaseAPI.insertData(connection, fileTable, headers, values);
+			DataFormatter dataFormatter = new DataFormatter();
+			// iterate through each row
+			String rowValues;
+			while (rowIterator.hasNext()) {
+	            row = rowIterator.next();
+	            rowValues = "";
+	            // iterate through each cell in the row
+	            cellIterator = row.cellIterator();
+	            while (cellIterator.hasNext()) {
+	                cell = cellIterator.next();
+	                String cellValue = dataFormatter.formatCellValue(cell);
+	                rowValues += ", '" + cellValue + "'";
+	            }
+	            rowValues = rowValues.substring(2);
+	            // add into table
+	            try {
+					databaseAPI.insertData(connection, fileTable, headers, rowValues);
 				} catch (Exception e) {
-					System.out.println("Error while inserting data for .csv file: " + fileName);
+					System.out.println("Error while inserting data for file: " + fileName);
 					e.printStackTrace();
 				}
-			}
+	        }
 		} catch (Exception e) {
-			System.out.println("Error while reading .csv file: " + fileName);
+			System.out.println("Error while reading file: " + fileName);
 			e.printStackTrace();
 		} finally {
 			try {
-				csvReader.close();
+				// close workbook
+				workbook.close();
 			} catch (IOException e) {
-				System.out.println("Error while closing .csv file: " + fileName);
+				System.out.println("Error while closing file: " + fileName);
 				e.printStackTrace();
 			}
 		}
