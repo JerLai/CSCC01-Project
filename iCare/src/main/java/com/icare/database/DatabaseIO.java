b9375b054579d1d3d3489996bf0ca4f6ecfd6376
VioletDuYue <Lhl:1123>
2018-11-11 23:41:25 -0500

    added export method to databaseIO

--- iCare/src/main/java/com/icare/database/DatabaseIO.java
+++ iCare/src/main/java/com/icare/database/DatabaseIO.java
@@ -1,12 +1,19 @@
 package main.java.com.icare.database;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class DatabaseIO {
+
+  private static final String DELIMITER = ";";
 
   public static boolean importData(Connection connection, File file) {
     // get file name and decide table name
@@ -142,6 +149,118 @@ public class DatabaseIO {
       }
     }
     return true;
+  }
+
+  public static File exportData(Connection connection, String fileName) {
+    String fileTable = fileName.substring(0, fileName.indexOf("."));
+    ArrayList<String> tableCols = new ArrayList<String>();
+    // get columnData for table
+    try {
+      tableCols = (ArrayList<String>) databaseAPI.getTableColumnData(connection,
+          fileTable);
+    } catch (Exception e) {
+      System.out.println(
+          "Error while checking table columns for .xlsx file: " + fileName);
+      e.printStackTrace();
+    }
+
+    Workbook workbook = new XSSFWorkbook();
+    Sheet sheet = workbook.createSheet(fileTable);
+
+    // set header font
+    Font headerFont = workbook.createFont();
+    headerFont.setBold(true);
+    headerFont.setFontHeightInPoints((short) 14);
+    // headerFont.setColor(IndexedColors.RED.getIndex());
+
+    // set header cell style
+    CellStyle headerCellStyle = workbook.createCellStyle();
+    headerCellStyle.setFont(headerFont);
+
+    // add the first row to write file name
+    Row firstRow = sheet.createRow(0);
+    Cell cell = firstRow.createCell(0);
+    cell.setCellValue(fileTable);
+    cell.setCellStyle(headerCellStyle);
+
+    // add the header row
+    Row headerRow = sheet.createRow(1);
+
+    for (int i = 1; i < tableCols.size(); i++) {
+      Cell headerCell = headerRow.createCell(i - 1);
+      int numOfBlk = tableCols.get(i).indexOf(" ");
+      headerCell.setCellValue(tableCols.get(i).substring(0, numOfBlk));
+      headerCell.setCellStyle(headerCellStyle);
+    }
+
+    // get data from table
+    ArrayList<String> dataArray = new ArrayList<String>();
+    String sql = "SELECT * FROM " + fileTable;
+    PreparedStatement preparedStatement;
+    try {
+      preparedStatement = connection.prepareStatement(sql);
+      ResultSet results = preparedStatement.executeQuery();
+      int numCols = results.getMetaData().getColumnCount();
+      while (results.next()) {
+        StringBuilder sb = new StringBuilder();
+        for (int m = 2; m <= numCols; m++) {
+          sb.append(String.format(String.valueOf(results.getString(m))));
+          if (m < numCols) {
+            sb.append(DELIMITER);
+          }
+        }
+        dataArray.add(sb.toString());
+      }
+      preparedStatement.close();
+    } catch (SQLException e) {
+      System.out.println("Error while writing to .xlsx file: " + fileName);
+      e.printStackTrace();
+    }
+
+    // iterate through each row and write to file
+    int rowNum = 2;
+    for (String row : dataArray) {
+      Row newRow = sheet.createRow(rowNum++);
+      int inOfDe = row.indexOf(DELIMITER);
+      int cellNum = 0;
+      while (inOfDe > 0) {
+        if (row.substring(0, inOfDe).equalsIgnoreCase("null")) {
+          newRow.createCell(cellNum).setCellValue("");
+        } else {
+          newRow.createCell(cellNum).setCellValue(row.substring(0, inOfDe));
+        }
+        row = row.substring(inOfDe + 1);
+        inOfDe = row.indexOf(DELIMITER);
+        cellNum++;
+      }
+      if (row.equalsIgnoreCase("null")) {
+        newRow.createCell(cellNum).setCellValue("");
+      } else {
+        newRow.createCell(cellNum).setCellValue(row);
+      }
+    }
+
+    for (int i = 0; i < tableCols.size(); i++) {
+      sheet.autoSizeColumn(i);
+    }
+
+
+    // Write the output to a file
+    File file = new File(fileTable + ".xlsx");
+    try {
+      FileOutputStream fileOut = new FileOutputStream(file);
+      workbook.write(fileOut);
+      fileOut.close();
+
+      // Closing the workbook
+      workbook.close();
+    } catch (IOException e) {
+      System.out.println("Error while closing .xlsx file: " + fileName);
+      e.printStackTrace();
+    }
+
+    return file;
+
   }
 
 }
