7c9cc0e8503dca3ce8ecc1b9c81b9e370cc1391d
Jeremy Lai <jeremylai.14@gmail.com>
2018-11-25 21:33:08 -0500

    Refactor of ChartGen.java, removing deprecated methods and adding Javadocs to new implementations for clarity

--- iCare/src/main/java/com/icare/gui/ChartGen.java
+++ iCare/src/main/java/com/icare/gui/ChartGen.java
@@ -1,9 +1,6 @@
 package main.java.com.icare.gui;
 
-import java.sql.Connection;
 import java.util.HashMap;
-import java.sql.SQLException;
-import java.util.Map.Entry;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -11,19 +8,18 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
-import org.jfree.data.general.PieDataset;
-import org.jfree.data.jdbc.JDBCPieDataset;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 
 /**
- * ChartPanel generator class that
+ * ChartPanel generator class that generates displayable Charts of several types
  *
  */
 public class ChartGen {
 
-
+	/**
+	 * Generates a bar chart given a dataset generated for a specific report
+	 * @param data the data to be represented by the chart in a CategoryDataset
+	 * @return the ChartPanel that displays the bar chart
+	 */
 	public static ChartPanel barChartGen(CategoryDataset data) {
 		JFreeChart barChart = ChartFactory.createBarChart("Sample Chart", "Category", "Score", data,
 				PlotOrientation.VERTICAL, true, true, false);
@@ -32,6 +28,11 @@ public class ChartGen {
 		return chartPanel;
 	}
 
+	/**
+	 * Generates a ChartPanel that displays a pie chart for a specific report
+	 * @param data the data to be represented by the chart in a DefaultPieDataset
+	 * @return the ChartPanel that displays the pie chart
+	 */
 	public static ChartPanel pieChartGen(DefaultPieDataset data) {
 		JFreeChart pieChart = ChartFactory.createPieChart("Sample Pie", // chart title
 				data, // data
@@ -41,139 +42,12 @@ public class ChartGen {
 		//chartPanel.setPreferredSize(parent.getDefaultSize());
 		return chartPanel;
 	}
-	//TODO: Add button to the ChartPanel to return to a menu?
 
 	/**
-	 * Generates a ChartPanel that displays a bar chart for a specific report
-	 * @param parent the parent frame that will contain the generated ChartPanel
-	 * @param parsedVals the values to insert to the chart parsed from the database from a specific query
-	 * @return a ChartPanel that displays the bar chart
+	 * Converts parsed data from query into a CategoryDataset to use for bar chart generation
+	 * @param data the parsed data
+	 * @return the CategoryDataset to be used in a bar chart
 	 */
-	public static ChartPanel barChartGen(GUI parent, String chartTitle, HashMap<String, Double> parsedVals) {
-		JFreeChart barChart = ChartFactory.createBarChart(chartTitle, "Category", "Score",
-				createBarDataset1(parsedVals), PlotOrientation.VERTICAL, true, true, false);
-
-		ChartPanel chartPanel = new ChartPanel(barChart);//ChartPanel extends JPanel
-		chartPanel.setPreferredSize(parent.getDefaultSize());
-		return chartPanel;
-	}
-
-	/**
-	 * Generates a ChartPanel that displays a bar chart for a specific report
-	 * @param parent the parent frame that will contain the generated ChartPanel
-	 * @param parsedVals the values to insert to the chart parsed from the database from a specific query
-	 * @return a ChartPanel that displays the bar chart
-	 */
-	public static ChartPanel barChartGen2(GUI parent, String chartTitle, HashMap<String, HashMap<String, Double>> parsedVals) {
-		JFreeChart barChart = ChartFactory.createBarChart(chartTitle, "Category", "Score",
-				createBarDataset2(parsedVals), PlotOrientation.VERTICAL, true, true, false);
-
-		ChartPanel chartPanel = new ChartPanel(barChart);//ChartPanel extends JPanel
-		chartPanel.setPreferredSize(parent.getDefaultSize());
-		return chartPanel;
-	}
-
-	/**
-	 * Generates a ChartPanel that displays a PiePlot for a specific report
-	 * 
-	 * @param parent     the parent frame that will contain the generated ChartPanel
-	 * @param chartTitle the title of the Chart
-	 * @param parsedVals the values to insert to the chart parsed from the database
-	 *                   from a specific query
-	 * @return a ChartPanel that displays the PiePlot
-	 */
-	public static ChartPanel pieChartGen(GUI parent, String chartTitle, HashMap<String, Double> parsedVals) {
-		JFreeChart pieChart = ChartFactory.createPieChart(chartTitle, // chart title
-				createPieDataset(parsedVals), // data
-				true, // include legend
-				true, false);
-		ChartPanel chartPanel = new ChartPanel(pieChart);
-
-		return chartPanel;
-	}
-
-	/**
-	 * Generates a ChartPanel that displays a PiePlot for a specific report given by
-	 * the query itself
-	 * 
-	 * @param parent     the parent frame that will contain the generated ChartPanel
-	 * @param connection the connection to the database
-	 * @param chartTitle the title of the Chart
-	 * @param query      the SQL query to generate the chart for
-	 * @return a ChartPanel that displays the PiePlot
-	 * @throws SQLException
-	 */
-	public static ChartPanel pieChartGen2(GUI parent, Connection connection, String chartTitle, String query)
-			throws SQLException {
-		JFreeChart pieChart = ChartFactory.createPieChart(chartTitle, // chart title
-				createPieDataset2(connection, query), // data
-				true, // include legend
-				true, false);
-		ChartPanel chartPanel = new ChartPanel(pieChart);
-
-		return chartPanel;
-	}
-
-	/**
-	 * Generates a ChartPanel that displays a line graph
-	 * @param parent
-	 * @return
-	 */
-	public static ChartPanel lineChartGen(GUI parent) {
-		JFreeChart xylineChart = ChartFactory.createXYLineChart("Sample Chart", "Category", "Score", createXYDataset(),
-				PlotOrientation.VERTICAL, true, true, false);
-		ChartPanel chartPanel = new ChartPanel(xylineChart);
-		chartPanel.setPreferredSize(parent.getDefaultSize());
-		return chartPanel;
-	}
-
-	/**
-	 * Generates the Dataset of values to be represented by a bar chart
-	 * @param parsedVals the values to create a Dataset for
-	 * @return a Dataset to be used by a bar chart
-	 */
-	private static CategoryDataset createBarDataset1(HashMap<String, Double> parsedVals) {
-		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-		for (Entry<String, Double> entry: parsedVals.entrySet()) {
-			dataset.setValue(entry.getValue(), entry.getKey(), "");
-		}
-		return dataset;
-	}
-	/**
-	 * Generates the Dataset of values to be represented by a bar chart DO NOT USE
-	 * @param parsedVals the values to create a Dataset for
-	 * @return a Dataset to be used by a bar chart
-	 */
-	private static CategoryDataset createBarDataset2(HashMap<String, HashMap<String, Double>> parsedVals) {
-		final String fiat = "FIAT";
-		final String audi = "AUDI";
-		final String ford = "FORD";
-		final String speed = "Speed";
-		final String millage = "Millage";
-		final String userrating = "User Rating";
-		final String safety = "safety";
-		final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-
-		
-		dataset.addValue(1.0, fiat, speed);
-		dataset.addValue(3.0, fiat, userrating);
-		dataset.addValue(5.0, fiat, millage);
-		dataset.addValue(5.0, fiat, safety);
-
-		dataset.addValue(5.0, audi, speed);
-		dataset.addValue(6.0, audi, userrating);
-		dataset.addValue(10.0, audi, millage);
-		dataset.addValue(4.0, audi, safety);
-
-		dataset.addValue(4.0, ford, speed);
-		dataset.addValue(2.0, ford, userrating);
-		dataset.addValue(3.0, ford, millage);
-		dataset.addValue(6.0, ford, safety);
-
-		return dataset;
-	}
-
-
 	public static CategoryDataset barDataConverter(HashMap<String, HashMap<String, Double>> data){
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 		for (String mainCat :data.keySet()){
@@ -184,6 +58,11 @@ public class ChartGen {
 		return dataset;
 	}
 
+	/**
+	 * Converts parsed data from query into a DefaultPieDataset to use for pie chart generation
+	 * @param data the parsed data
+	 * @return the DefaultPieDataset to be used in a pie chart
+	 */
 	public static DefaultPieDataset pieDataConverter(HashMap<String, Double> data){
 		DefaultPieDataset dataset = new DefaultPieDataset();
 		for (String key :data.keySet())
@@ -191,60 +70,4 @@ public class ChartGen {
 		return dataset;
 	}
 
-
-	/**
-	 * Generates the Dataset of values to be represented by a pie chart
-	 * @param parsedVals the values to create a Dataset for
-	 * @return a dataset to be used by a pie chart
-	 */
-	private static PieDataset createPieDataset(HashMap<String, Double> parsedVals) {
-		DefaultPieDataset dataset = new DefaultPieDataset();
-		for (String category : parsedVals.keySet()) {
-			dataset.setValue(category, parsedVals.get(category));
-		}
-//		dataset.setValue("IPhone 5s", new Double(20)); // Key, Value relation
-//		dataset.setValue("SamSung Grand", new Double(20));
-//		dataset.setValue("MotoG", new Double(40));
-//		dataset.setValue("Nokia Lumia", new Double(10));
-		return dataset;
-	}
-
-	/**
-	 * Proof of concept DO NOT USE
-	 * @param connection
-	 * @param query
-	 * @return
-	 * @throws SQLException
-	 */
-	private static PieDataset createPieDataset2(Connection connection, String query) throws SQLException {
-		JDBCPieDataset dataset = new JDBCPieDataset(connection, query);
-//		dataset.setValue("IPhone 5s", new Double(20)); // Key, Value relation
-//		dataset.setValue("SamSung Grand", new Double(20));
-//		dataset.setValue("MotoG", new Double(40));
-//		dataset.setValue("Nokia Lumia", new Double(10));
-		return dataset;
-	}
-
-	/**
-	 * Proof of concept DO NOT USE
-	 * @return
-	 */
-	private static XYDataset createXYDataset() {
-		final XYSeries firefox = new XYSeries("Firefox");
-		firefox.add(1.0, 1.0);//(x value and y value respectively)
-		firefox.add(2.0, 4.0);
-		firefox.add(3.0, 3.0);
-
-		final XYSeries chrome = new XYSeries("Chrome");
-		chrome.add(1.0, 4.0);
-		chrome.add(2.0, 5.0);
-		chrome.add(3.0, 6.0);
-
-		final XYSeries iexplorer = new XYSeries("InternetExplorer");
-		iexplorer.add(3.0, 4.0);
-		iexplorer.add(4.0, 5.0);
-		iexplorer.add(5.0, 4.0);
-		XYSeriesCollection dataset = new XYSeriesCollection();
-		return dataset;
-	}
 }
